18eae39dabcfc861501ad3a5f2c75fc5626d438d
Fred <fred22855@hotmail.com.tw>
2023-04-10 20:02:38 +0800

    update todolist funtion delLI

--- toDoListAdd.js
+++ toDoListAdd.js
@@ -1,33 +1,57 @@
 //註冊新增事項按鈕事件
 var addBtn = document.getElementById('btnInput')
-addBtn.addEventListener("click",AddList)
+addBtn.addEventListener("click",Keyin)
+var tbInp = document.getElementById('tbInput')
+// tbInp.addEventListener("click",KeyReady)
 
-//註冊動態式遍歷所有的刪除按鈕事件
-var delBtn= document.querySelectorAll('.delList')
-for(i =0; i < delBtn.length;i++){
-    delBtn[i].addEventListener("click",DelList)
+function Keyin(){
+    if (tbInp.value === ""){
+        alert("未輸入事項內容!");
+    }
+    else{
+        AddList();
+    }
 }
-
 
 //新增LI項目按鈕的方法
 function AddList(){
-    var tInput =document.getElementById('tbInput');
+    // var inpBtn =document.getElementById('tbInput');
     var targetUl=document.getElementById('listUL')
     
     
-    
+    //為LI填入按鈕與內容
     var addLi = document.createElement('li');
     var addBtn = document.createElement('button');
     addBtn.setAttribute("class","delList");
     addBtn.innerText="刪除這則事項";
-    addLi.innerHTML=tInput.value;
+    addLi.innerHTML=tbInp.value;
 
+    //錄入DOM內容
     addLi.append(addBtn);
     targetUl.append(addLi);
 
+    //為按鈕註冊刪除事件
+    addBtn.addEventListener("click",DelList);
 }
 
 
-function DelList(){
-
+function DelList(){ 
+    var listNode=this.parentNode;
+    listNode.remove();
 }
+
+
+
+
+
+//載入時遍歷所有的按鈕註冊刪除事件
+// var delBtn= document.querySelectorAll('.delList')
+// for(i =0; i < delBtn.length;i++){
+//     delBtn[i].addEventListener("click",DelList)
+// }
+
+// function KeyReady(){
+//     if(tbInp.value == "在此輸入事情內容"){
+//     tbInp.value = "";
+//     }
+// }
